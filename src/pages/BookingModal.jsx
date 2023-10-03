c81bb2d510f2a31a594db1d1c41e7ab6822c71e2
barteek-poz <szowior4@wp.pl>
2023-10-03 23:41:09 +0200

    fixing styling

--- src/pages/BookingModal.jsx
+++ src/pages/BookingModal.jsx
@@ -2,14 +2,16 @@ import styles from "./BookingModal.module.css";
 import React, { useContext, useState } from "react";
 import { createPortal } from "react-dom";
 import Select from "react-select";
-import { useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
 import bookingImg from "../img/booking.jpg";
 import Button from "../components/Button";
 import ModalContext from "../context/modalContext";
 
 const Backdrop = (props) => {
   return (
-    <div className={styles.modalBackdrop} onClick={props.onCloseModalHandler}></div>
+    <div
+      className={styles.modalBackdrop}
+      onClick={props.onCloseModalHandler}></div>
   );
 };
 
@@ -18,9 +20,9 @@ const ModalOverlay = (props) => {
   const [model, setModel] = useState(null);
   const [errorSalon, setErrorSalon] = useState(false);
   const [errorModel, setErrorModel] = useState(false);
-  const [formValid, setFormIsValid] = useState(false)
+  const [formValid, setFormIsValid] = useState(false);
 
-  const ctx = useContext(ModalContext) // ustalic dlaczego nie dziala props na onCloseModal
+  const ctx = useContext(ModalContext); // ustalic dlaczego nie dziala props na onCloseModal
 
   const carModels = [
     { value: "Lena", label: "Lena" },
@@ -45,7 +47,7 @@ const ModalOverlay = (props) => {
   const selectStyles = {
     control: (styles) => ({
       ...styles,
-      width: "90%",
+      width: "100%",
       borderRadius: "5px",
       fontSize: "1.6rem",
     }),
@@ -60,7 +62,7 @@ const ModalOverlay = (props) => {
 
   const modelChangeHandler = (selectedModel) => {
     setModel(selectedModel);
-    setErrorModel(false)
+    setErrorModel(false);
   };
 
   const onSubmitForm = (data) => {
@@ -74,116 +76,126 @@ const ModalOverlay = (props) => {
       setModel(null);
       setSalon(null);
       reset();
-      setFormIsValid(true)
+      setFormIsValid(true);
     }
   };
 
   return (
     <div className={styles.backdrop}>
-<div className={styles.formWrapper}>
-      <form className={styles.formInputs}>
-        <div className={styles.formHeader}>
-          <h2 className={styles.formHeading}>Zarezerwuj jazdę próbną w PIRX</h2>
-          <p>
-            Podaj swoje dane kontaktowe, następnie wybierz model auta oraz salon
-            w którym chciałbyś odbyć jazdę próbną. Nasz konsultant skontaktuje
-            się z Tobą, aby wybrać dogodny dla Ciebie termin.{" "}
-          </p>
-        </div>
-        <div className={styles.inputBox}>
-          <input
-            className={styles.formInput}
-            placeholder="Imię"
-            type="text"
-            name="name"
-            {...register("name", {
-              required: "Name is required",
-            })}></input>
-          {errors.name && errors.name.type === "required" && (
-            <p className={styles.errorInput}>Name is required</p>
-          )}
-        </div>
-        <div className={styles.inputBox}>
-          <input
-            className={styles.formInput}
-            placeholder="Nazwisko"
-            type="text"
-            name="surname"
-            {...register("name", {
-              required: "Name is required",
-            })}></input>
-          {errors.name && errors.name.type === "required" && (
-            <p className={styles.errorInput}>Name is required</p>
-          )}
-        </div>
-        <div className={styles.inputBox}>
-          <input
-            className={styles.formInput}
-            placeholder="Email"
-            type="text"
-            name="email"
-            {...register("email", {
-              required: "Email is requierd",
-              pattern: {
-                value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
-                message: "Wymagany format : xxx@xx.xx",
-              },
-            })}></input>
-          {errors.email && errors.email.type === "required" && (
-            <p className={styles.errorInput}>Podaj adres email</p>
-          )}
-          {errors.email && errors.email.type === "pattern" && (
-            <p className={styles.errorInput}>{errors.email.message}</p>
-          )}
-        </div>
-        <div className={styles.inputBox}>
-          <input
-            className={styles.formInput}
-            placeholder="Telefon"
-            type="number"
-            name="phone"
-            {...register("phone", {
-              required: "Podaj numer telefonu",
-              minLength: {
-                value: 9,
-                message: "Numer powinien mieć 9 cyfr",
-              },
-            })}></input>
-          {errors.phone && errors.phone.type && (
-            <p className={styles.errorInput}>{errors.phone.message}</p>
-          )}
-        </div>
-        <div className={styles.inputBox}>
-          <Select
-            placeholder="Wybierz salon"
-            value={salon}
-            onChange={salonChangeHandler}
-            options={carSalons}
-            styles={selectStyles}></Select>
-          {errorSalon && <p className={styles.errorInput}>Wybierz salon</p>}
-        </div>
-        <div className={styles.inputBox}>
-          <Select
-            placeholder="Wybierz model"
-            value={model}
-            onChange={modelChangeHandler}
-            options={carModels}
-            styles={selectStyles}></Select>
+      <div className={styles.formWrapper}>
+        <form className={styles.formInputs}>
+          <div className={styles.formHeader}>
+            <h2 className={styles.formHeading}>
+              Zarezerwuj jazdę próbną w PIRX
+            </h2>
+            <p>
+              Podaj swoje dane kontaktowe, następnie wybierz model auta oraz
+              salon w którym chciałbyś odbyć jazdę próbną. Nasz konsultant
+              skontaktuje się z Tobą, aby wybrać dogodny dla Ciebie termin.{" "}
+            </p>
+          </div>
+          <div className={styles.inputBox}>
+            <input
+              className={styles.formInput}
+              placeholder="Imię"
+              type="text"
+              name="name"
+              {...register("name", {
+                required: "Name is required",
+              })}></input>
+            {errors.name && errors.name.type === "required" && (
+              <p className={styles.errorInput}>Name is required</p>
+            )}
+          </div>
+          <div className={styles.inputBox}>
+            <input
+              className={styles.formInput}
+              placeholder="Nazwisko"
+              type="text"
+              name="surname"
+              {...register("name", {
+                required: "Name is required",
+              })}></input>
+            {errors.name && errors.name.type === "required" && (
+              <p className={styles.errorInput}>Name is required</p>
+            )}
+          </div>
+          <div className={styles.inputBox}>
+            <input
+              className={styles.formInput}
+              placeholder="Email"
+              type="text"
+              name="email"
+              {...register("email", {
+                required: "Email is requierd",
+                pattern: {
+                  value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                  message: "Wymagany format : xxx@xx.xx",
+                },
+              })}></input>
+            {errors.email && errors.email.type === "required" && (
+              <p className={styles.errorInput}>Podaj adres email</p>
+            )}
+            {errors.email && errors.email.type === "pattern" && (
+              <p className={styles.errorInput}>{errors.email.message}</p>
+            )}
+          </div>
+          <div className={styles.inputBox}>
+            <input
+              className={styles.formInput}
+              placeholder="Telefon"
+              type="number"
+              name="phone"
+              {...register("phone", {
+                required: "Podaj numer telefonu",
+                minLength: {
+                  value: 9,
+                  message: "Numer powinien mieć 9 cyfr",
+                },
+              })}></input>
+            {errors.phone && errors.phone.type && (
+              <p className={styles.errorInput}>{errors.phone.message}</p>
+            )}
+          </div>
+          <div className={styles.inputBox}>
+            <Select
+              placeholder="Wybierz salon"
+              value={salon}
+              onChange={salonChangeHandler}
+              options={carSalons}
+              styles={selectStyles}></Select>
+            {errorSalon && <p className={styles.errorInput}>Wybierz salon</p>}
+          </div>
+          <div className={styles.inputBox}>
+            <Select
+              placeholder="Wybierz model"
+              value={model}
+              onChange={modelChangeHandler}
+              options={carModels}
+              styles={selectStyles}></Select>
             {errorModel && <p className={styles.errorInput}>Wybierz model</p>}
-        </div>
-       {formValid && <p className={styles.formConfirm}>Formularz został wysłany. Nasz konsultant niedługo skontakuje się z Tobą w celu umówienia jazdy próbnej.</p>}
-        <div className={styles.formBtns}>
-          {!formValid && <Button onClick={handleSubmit(onSubmitForm)} >Wyślij</Button>}
-         {!formValid && <Button onClick={ctx.onCloseModalHandler}>Anuluj</Button>}
-          {formValid && <Button >Zamknij</Button>}
-        </div>
-      </form>
-      <div
-        className={styles.bookingImg}
-        style={{ backgroundImage: `url(${bookingImg})` }}></div>
+          </div>
+          {formValid && (
+            <p className={styles.formConfirm}>
+              Formularz został wysłany. Nasz konsultant niedługo skontakuje się
+              z Tobą w celu umówienia jazdy próbnej.
+            </p>
+          )}
+          <div className={styles.formBtns}>
+            {!formValid && (
+              <Button onClick={handleSubmit(onSubmitForm)}>Wyślij</Button>
+            )}
+            {!formValid && (
+              <Button onClick={ctx.onCloseModalHandler}>Anuluj</Button>
+            )}
+            {formValid && <Button>Zamknij</Button>}
+          </div>
+        </form>
+        <div
+          className={styles.bookingImg}
+          style={{ backgroundImage: `url(${bookingImg})` }}></div>
+      </div>
     </div>
-    </div>
-    
   );
 };
 
